03f0d3cb0a49646b264c3f54e9e481cfb9a65ce6
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-11 12:21:10 +0800

    Pass exercise requirement

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    let letters = [];
 
+    for (let i = word.length - 1; i >= 0; i--) {
+        letters.push(word[i]);
+    }
+
+    return letters.join("");
 };
-
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
