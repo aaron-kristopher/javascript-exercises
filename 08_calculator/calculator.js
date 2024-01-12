9cbb19c1c4848bea1729b91fa2cd561cb51063fb
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-12 19:27:59 +0800

    Pass exercise requirement

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,53 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function (nums) {
+    let product = 1;
 
+    for (let i = 0; i < nums.length; i++) {
+        product *= nums[i];
+    }
+
+    return product;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+    return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0) return 1;
+
+    let answer = 1;
+
+    for (let i = num; i > 0; i--) {
+        answer *= i;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
