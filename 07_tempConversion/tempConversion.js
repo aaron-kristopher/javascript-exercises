0166375b7f6c98390f8f77700bd25ffa6212a141
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-12 19:04:07 +0800

    Pass exercise requirement

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+    // Formula: °C = 5/9 × (°F - 32)
+    let celcius = (5 / 9) * (farenheit - 32);
+    return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+    // Formula: °F = (°C × 9/5) + 32
+    let farenheit = (celcius * (9 / 5) + 32).toFixed(1);
+    return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
