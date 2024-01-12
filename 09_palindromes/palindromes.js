de57711a48acbe73fe74e33a83858f6a95ee5c80
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-12 21:24:24 +0800

    Pass exercise requirement

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let letters = sentence
+        .toLowerCase()
+        .split("")
+        .filter((letter) => letter.match(/[a-z0-9]/i));
 
+    return letters.join("") === letters.reverse().join("");
 };
 
 // Do not edit below this line
