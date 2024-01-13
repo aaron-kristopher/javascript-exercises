51a1a42cf3a794408c3370f62347c01fcb1fe35b
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-01-13 16:15:59 +0800

    Pass exercise requirement

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);        
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);        
 
+        return (oldestAge < currentAge) ? current : oldest;
+    });
 };
+
+const getAge = function(born, died) {
+    died = died ?? new Date().getFullYear();
+    return died - born; 
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
